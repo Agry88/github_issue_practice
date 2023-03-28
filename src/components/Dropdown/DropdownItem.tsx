8dc9787d245402b4a07bbf41254e7df4c522726d
agry88 <rgok307085@gmail.com>
2023-03-28 16:18:40 +0800

    Fix: make component can receive click event.

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 export type DropDownItemProp = {
   children: React.ReactNode;
   isSelected?: boolean
+  onClick?: () => void;
 };
 
-export default function DropDownItem({ children, isSelected }: DropDownItemProp) {
+export default function DropDownItem({ children, isSelected, onClick }: DropDownItemProp) {
   return (
     <li>
-      <span className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white ${isSelected ? 'bg-gray-300' : ''}`}>
+      <button
+        type="button"
+        onClick={onClick}
+        className={`w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white ${isSelected ? 'bg-gray-300' : ''}`}
+      >
         {children}
-      </span>
+      </button>
     </li>
   );
 }
