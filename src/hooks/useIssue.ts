36ec7b5f7be3bb6b394cf816650f916207150b97
Agry88 <rgok307085@gmail.com>
2023-03-29 19:55:53 +0800

    Fix: prevent some issue don't have tags.

--- src/hooks/useIssue.ts
+++ src/hooks/useIssue.ts
@@ -5,19 +5,22 @@ async function getListIssue(): Promise<Issue[]> {
   const response = await fetch('https://api.github.com/repos/Agry88/github_issue_practice/issues');
   const data = await response.json();
 
-  const issueList: Issue[] = data.map((issue: any) => ({
-    issueId: issue.id,
-    title: issue.title,
-    body: issue.body,
-    tag: {
-      tagId: issue.labels[0].id,
-      tagName: issue.labels[0].name,
-      tagColor: issue.labels[0].color,
-    },
-    creatorId: issue.user.id,
-    creatorName: issue.user.login,
-    creatorAvatar: issue.user.avatar_url,
-  }));
+  const issueList: Issue[] = data.map((issue: any) => {
+    if (issue.labels.length === 0) return null;
+    return {
+      issueId: issue.id,
+      title: issue.title,
+      body: issue.body,
+      tag: {
+        tagId: issue.labels[0].id,
+        tagName: issue.labels[0].name,
+        tagColor: issue.labels[0].color,
+      },
+      creatorId: issue.user.id,
+      creatorName: issue.user.login,
+      creatorAvatar: issue.user.avatar_url,
+    };
+  });
 
   return issueList;
 }
@@ -27,7 +30,8 @@ export default function useIssue() {
 
   useEffect(() => {
     getListIssue().then((issueListData: Issue[]) => {
-      setIssueList(issueListData);
+      const filteredIssueListData = issueListData.filter((issue: Issue) => issue !== null);
+      setIssueList(filteredIssueListData);
     });
   }, []);
 
