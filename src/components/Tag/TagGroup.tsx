dcd246f055be4079edfaf9687d78f4f4272d882b
Agry88 <rgok307085@gmail.com>
2023-03-30 16:52:20 +0800

    Feat: make tag group more responsive.

--- src/components/Tag/TagGroup.tsx
+++ src/components/Tag/TagGroup.tsx
@@ -10,14 +10,14 @@ export default function TagGroup({ selectedTag, setSelectedTag }: Props) {
   const tagArray: Tag[] = ['Open', 'In Progress', 'Done'];
 
   return (
-    <div className="mb-4">
+    <div className="w-1/4 max-w-xl mb-4">
       <div className="inline-flex rounded-md shadow-sm" role="group">
         {tagArray.map((name) => (
           <button
             type="button"
             key={name}
             onClick={() => setSelectedTag(name)}
-            className={`px-4 py-2 ${selectedTag === name ? 'bg-gray-900 text-white' : 'bg-transparent'} text-sm font-medium border border-gray-900 first:rounded-l-lg last:rounded-r-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white`}
+            className={`px-2 py md:px-4 md:py-2 ${selectedTag === name ? 'bg-gray-900 text-white' : 'bg-transparent'} text-xs sm:text-sm font-medium border border-gray-900 first:rounded-l-lg last:rounded-r-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white`}
           >
             {name}
           </button>
