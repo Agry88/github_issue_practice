31bf27d84ca63b55772edf65b9b228d8f89caed8
agry88 <rgok307085@gmail.com>
2023-04-03 20:54:59 +0800

    Fix: wrong redirect path after login.

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -8,7 +8,7 @@ export default function RedirectPage({ acessToken }: { acessToken: string }) {
 
   useEffect(() => {
     localStorage.setItem('accessToken', acessToken);
-    router.push('/issues');
+    router.push('/issue');
   }, [acessToken, router]);
 
   return (
