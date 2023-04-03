e5c37dee2324d1e03ad872e24e06bd19351a1653
agry88 <rgok307085@gmail.com>
2023-04-03 23:22:00 +0800

    Fix: vercel deployment error.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const isDev = process.env.NODE_ENV === 'development';
 
-const hostURL = isDev ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+const hostURL = !process.env.NEXT_PUBLIC_VERCEL_URL ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export {
   isDev,
