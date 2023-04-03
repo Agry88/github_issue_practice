f8e90201336a5b778b8139a154a7486447ff5f37
agry88 <rgok307085@gmail.com>
2023-04-03 22:32:38 +0800

    Feat: click navbar buttons to navigate.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Icon } from '@iconify/react';
+import { useRouter } from 'next/router';
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    router.push('/signup');
+  };
+
   return (
     <nav className="bg-primary flex flex-row justify-between items-center px-2 py-5 w-full h-20 shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]">
-      <div className="flex flex-row items-center">
+      <button
+        type="button"
+        className="flex flex-row items-center"
+        onClick={() => router.push('/issue')}
+      >
         <Icon icon="mdi:github" color="white" fontSize={40} />
         <span className="font-sans text-3xl font-normal text-white">Github-Issues-practice</span>
-      </div>
-      <div className="flex flex-row items-center">
+      </button>
+      <button
+        type="button"
+        className="flex flex-row items-center"
+        onClick={handleLogout}
+      >
         <Icon icon="material-symbols:logout" color="white" fontSize={40} />
         <span className="font-sans text-2xl font-normal text-white">Logout</span>
-      </div>
+      </button>
     </nav>
   );
 }
